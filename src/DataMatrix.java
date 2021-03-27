155e5a507434f166d11c163e5a70ed05fcdbdb66
Michael Hackett <mhackett909@gmail.com>
2021-03-27 16:06:09 -0700

    Minor tweaks

--- src/DataMatrix.java
+++ src/DataMatrix.java
@@ -13,12 +13,12 @@ public class DataMatrix implements BarcodeIO {
    
    public DataMatrix(BarcodeImage image) {
       text = "";
-      this.scan(image);
+      scan(image);
    }
    
    public DataMatrix(String text) {
       image = new BarcodeImage();
-      this.text = text;
+      readText(text);
    }
    
     @Override
@@ -30,14 +30,19 @@ public class DataMatrix implements BarcodeIO {
           this.actualWidth = this.computeSignalWidth();
           return true;
        }
-       catch(Exception e) {
+       catch(CloneNotSupportedException e) {
           return false;
        }
     }
     
    @Override
     public boolean readText(String text) {
-        return false;
+       if (text == null || text.equals("") ||
+               text.length() > (BarcodeImage.MAX_WIDTH-2)) return false;
+       else {
+           this.text = text;
+           return true;
+       }
     }
 
     @Override
@@ -47,6 +52,7 @@ public class DataMatrix implements BarcodeIO {
 
     @Override
     public boolean translateImageToText() {
+        //TODO Michael
         return false;
     }
     
@@ -57,11 +63,11 @@ public class DataMatrix implements BarcodeIO {
     
     @Override
     public void displayImageToConsole() {
-
+        //TODO Michael
     }
     
     private void cleanImage() {
-       // TODO Auto-generated method stub
+        //TODO Michael
     }
     
     private int computeSignalWidth() {
@@ -84,5 +90,76 @@ public class DataMatrix implements BarcodeIO {
              break;
        }
        return height;
+    }
+    public int getActualWidth() { return actualWidth; }
+    public int getActualHeight() { return actualHeight; }
+    public static void main(String[] args)
+    {
+        String[] sImageIn =
+                {
+                        "                                               ",
+                        "                                               ",
+                        "                                               ",
+                        "     * * * * * * * * * * * * * * * * * * * * * ",
+                        "     *                                       * ",
+                        "     ****** **** ****** ******* ** *** *****   ",
+                        "     *     *    ****************************** ",
+                        "     * **    * *        **  *    * * *   *     ",
+                        "     *   *    *  *****    *   * *   *  **  *** ",
+                        "     *  **     * *** **   **  *    **  ***  *  ",
+                        "     ***  * **   **  *   ****    *  *  ** * ** ",
+                        "     *****  ***  *  * *   ** ** **  *   * *    ",
+                        "     ***************************************** ",
+                        "                                               ",
+                        "                                               ",
+                        "                                               "
+
+                };
+
+
+
+        String[] sImageIn_2 =
+                {
+                        "                                          ",
+                        "                                          ",
+                        "* * * * * * * * * * * * * * * * * * *     ",
+                        "*                                    *    ",
+                        "**** *** **   ***** ****   *********      ",
+                        "* ************ ************ **********    ",
+                        "** *      *    *  * * *         * *       ",
+                        "***   *  *           * **    *      **    ",
+                        "* ** * *  *   * * * **  *   ***   ***     ",
+                        "* *           **    *****  *   **   **    ",
+                        "****  *  * *  * **  ** *   ** *  * *      ",
+                        "**************************************    ",
+                        "                                          ",
+                        "                                          ",
+                        "                                          ",
+                        "                                          "
+
+                };
+
+        BarcodeImage bc = new BarcodeImage(sImageIn);
+        DataMatrix dm = new DataMatrix(bc);
+        
+        // First secret message
+        dm.translateImageToText();
+        dm.displayTextToConsole();
+        dm.displayImageToConsole();
+
+        // second secret message
+        bc = new BarcodeImage(sImageIn_2);
+        dm.scan(bc);
+        dm.translateImageToText();
+        dm.displayTextToConsole();
+        dm.displayImageToConsole();
+
+        // create your own message
+        dm.readText("What a great resume builder this is!");
+        dm.generateImageFromText();
+        dm.displayTextToConsole();
+        dm.displayImageToConsole();
+
+
     }
 }
